6ed553e23766bbbf1f8e152568808e115cc17f70
Yavor Shahpasov <yavosh@users.noreply.github.com>
2019-02-18 16:11:16 +0200

    Correct position for VesselName
    according to https://www.navcen.uscg.gov/?pageName=AISMessagesB

--- staticdatareport.go
+++ staticdatareport.go
@@ -53,15 +53,11 @@ func DecodeStaticDataReport(payload string) (StaticDataReport, error) {
 	}
 
 	m.Repeat = uint8(bitsToInt(6, 7, data))
-
-	//m.MMSI = uint32(decodeAisChar(data[1]))<<28>>2 | uint32(decodeAisChar(data[2]))<<20 |
-	//	uint32(decodeAisChar(data[3]))<<14 | uint32(decodeAisChar(data[4]))<<8 |
-	//	uint32(decodeAisChar(data[5]))<<2 | uint32(decodeAisChar(data[6]))>>4
 	m.MMSI = bitsToInt(8, 37, data)
 
 	m.PartNo = uint8(bitsToInt(38, 39, data))
 	if m.PartNo == 0 {
-		m.VesselName = bitsToString(112, 231, data)
+		m.VesselName = bitsToString(40, 161, data)
 	} else {
 		m.ShipType = uint8(bitsToInt(40, 47, data))
 		m.VendorID = bitsToString(48, 65, data)
